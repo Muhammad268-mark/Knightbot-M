626c7fe1f0a81c3cdae981b319c95818de2aaca0
Muhammad268-mark <muhammad95js@gmail.com>
2025-05-19 07:30:41 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '923242280954', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'M.A Legend', // Your name
+  ownerNumber: '923499746839', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
